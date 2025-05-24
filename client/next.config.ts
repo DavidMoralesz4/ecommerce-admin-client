991b066256299c077adaa97c0f371ff8cc979918
DavidMoralesz4 <jd3285201@gmail.com>
2025-05-24 13:05:33 -0500

    adjustin image domains

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,11 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // images: {
-  //   domains: ["tse1.mm.bing.net", "www.somosmamas.com.ar", "cloudinary.com", "imgur.com", "http2.mlstatic.com", "www.uncolor.com.co", "ss402.liverpool.com.mx", ],
-  // },
   images: {
-    disableStaticImages: true, // ⚠️ Desactiva el componente Image
+    domains: ["tse1.mm.bing.net", "www.somosmamas.com.ar", "cloudinary.com", "imgur.com", "http2.mlstatic.com", "www.uncolor.com.co", "ss402.liverpool.com.mx", ],
   },
 };
 
